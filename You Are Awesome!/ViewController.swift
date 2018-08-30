6b0996f51222a389a9f17363797f868d9adda50f
Jolene Lozano <lozanoja@bc.edu>
2018-08-30 13:54:41 -0400

    Initial Commit

---
+++ You Are Awesome!/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  You Are Awesome!
+//
+//  Created by J. Lozano on 8/30/18.
+//  Copyright © 2018 J. Lozano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
